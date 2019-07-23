351381e19fc5fa1e8aed7d9f8fe8f3c21a2b1da0
Florian Pubert <florain.pubert@orange.com>
2019-07-23 22:58:25 +0200

    [Angular] Update language change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import {Constants} from "./constants/constants";
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,31 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class AppComponent {
 
+  private currentLang: string = Constants.LOCALE_FR;
+
   constructor(private translate: TranslateService) {
 
-    translate.addLangs(['en', 'fr']);
+    // app's languages
+    translate.addLangs([Constants.LOCALE_EN, Constants.LOCALE_FR]);
 
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+    translate.setDefaultLang(Constants.LOCALE_FR);
   }
 
+  /**
+   * Set the language translation.
+   * @param language The language to set
+   */
   useLanguage(language: string) {
     this.translate.use(language);
+    this.currentLang = language;
   }
 
-  isCurrentLanguage(language: string):boolean {
-    return this.translate.currentLang == language;
+  getUnusedLanguage(): string {
+    if (this.currentLang == Constants.LOCALE_FR) {
+      return Constants.LOCALE_EN;
+    } else {
+      return Constants.LOCALE_FR;
+    }
   }
 }
